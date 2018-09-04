23ed764311347426926723142e0030ab4d60f55f
Styrkar Blaer <stykki@hotmail.com>
2018-09-04 14:38:03 +0000

    testing for loops

--- hopaverk5/max_int.py
+++ hopaverk5/max_int.py
@@ -1,1 +1,10 @@
-print("'It just works' - Todd Howard")
+num_int = int(input("Input a number: "))    # Do not change this line
+max_int = 0
+while num_int > 0:
+    num_int = int(input("Input a number: "))    # Do not change this line
+
+    for i in range (i, num_int):
+        if i > num_int:
+            max_int = i
+
+print("The maximum is", max_int)    # Do not change this line
