27b8a1f621c51514e6a1a2a4ce9ee2e3095c7da9
Styrkar Blaer <stykki@hotmail.com>
2018-09-04 14:46:03 +0000

    Solved

--- hopaverk5/max_int.py
+++ hopaverk5/max_int.py
@@ -3,8 +3,7 @@ max_int = 0
 while num_int > 0:
     num_int = int(input("Input a number: "))    # Do not change this line
 
-    for i in range (i, num_int):
-        if i > num_int:
-            max_int = i
+    if num_int > max_int:
+        max_int = num_int
 
 print("The maximum is", max_int)    # Do not change this line
