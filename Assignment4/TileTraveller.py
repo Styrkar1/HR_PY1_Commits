c593134c4ccfd1449d0d3a1743b56255cb7862ff
Styrkar Blaer <stykki@hotmail.com>
2018-09-18 12:59:16 +0000

    Spaghettifacation is nearing completion

--- Assignment4/TileTraveller.py
+++ Assignment4/TileTraveller.py
@@ -6,8 +6,73 @@ while True:
     else:
         if tile == 1.1:
             tule = input("You can travel: (N)orth")
+            if tule.upper() == "N":
+                print("Direction: N")
+                tile = 1.2
+            else:
+                print("Not a valid direction!")
 
-label1:
-print("You can travel: (N)orth")
-label2:
-print("You can travel: (N)orth or (E)ast")
+        elif tile == 1.2:
+            tule = input("You can travel: (N)orth or (E)ast or (S)outh")
+            if tule.upper() == "N"
+                print("Direction: N")
+                tile = 1.3
+            elif tule.upper() == "E":
+                print("Direction: S")
+                tile = 1.1
+            
+            
+        elif tile == 1.3:
+            tule = input("You can travel:(E)ast or (S)outh")
+            if tule.upper() == "E"
+                print("Direction: E")
+                tile = 2.3
+            elif tule.uppet == "S"
+                print("Direction:S")
+                tile = 1.2
+
+        elif tile == 2.1:
+            tule = input("You can travel: (N)orth")
+            if tule.upper() == "N":
+                print("Direction: N")
+                tile = 2.2
+
+        elif tile == 2.2:
+            tule = input("You can travel: (W)est or (S)outh")
+            if tule.upper() == "W":
+                print("Direction: W")
+                tile = 1.2
+            elif tule.upper() == "S":
+                print("Direction: S")
+                tile = 2.1
+
+        elif tile == 2.3:
+            tule = input("You can travel: (E)ast or (W)est")
+            if tule.upper == "E":
+                print("Direction: E")
+                tile = 3.3
+            elif tule.upper() == "W":
+                print("Direction: W")
+                tile = 1.3
+
+        elif tile == 3.2:
+            tule = input("You can travel: (N)orth or (S)outh")
+            if tule.upper() == "N":
+                print("Direction: N")
+                tile = 3.3
+            if tule.upper() == "S":
+                print("Direction: S")
+                tile = 3.1
+
+        elif tile == 3.3:
+            tule = input("You can travel: (W)est or (S)outh")
+            if tule.upper() == "W":
+                print("Direction: W")
+                tile = 2.3
+            elif tule.upper() = "S":
+                print("Direction: S")
+                tile = 3.2
+
+        else:
+            print("Unknown, Breaking")
+            break
