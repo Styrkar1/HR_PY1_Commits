53eb53784f9fccdca1d1567858925884cd99db4d
Styrkar Blaer <stykki@hotmail.com>
2018-09-18 12:43:51 +0000

    Spaghetti code INITIALIZED

---
+++ Assignment4/TileTraveller.py
@@ -0,0 +1,13 @@
+tile = 1.1
+while True:
+    if tile == 3.1:
+        print("Victory!")
+        break
+    else:
+        if tile == 1.1:
+            tule = input("You can travel: (N)orth")
+
+label1:
+print("You can travel: (N)orth")
+label2:
+print("You can travel: (N)orth or (E)ast")
