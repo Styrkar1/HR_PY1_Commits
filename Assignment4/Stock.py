e6dff3fd71ee4a989f591ecf0b9f36942e9b0862
Styrkar Blaer <stykki@hotmail.com>
2018-09-20 12:38:07 +0000

    oof

--- Assignment4/Stock.py
+++ Assignment4/Stock.py
@@ -1,16 +1,28 @@
-shares = int(input("Number of shares: "))
+while True:
+    shares = input("Number of shares: ")
 
-price = input("Enter a price (dollars, numerator, deniminator): ")
-
-dollar, nume, denom = price.split(" ")
+    if shares.isalpha():
+        print("Invalid number!")
+        continue
+    else:
+        break
 
 while True:
-    print(dollar," ",denom," ", nume)
+    price = input("Enter a price (dollars, numerator, deniminator): ")
+    dollar, nume, denom = price.split(" ")
 
-    try:
-        val = str(dollar)
-        val = str(nume)
-        val = str(denom)
-    except ValueError:
-        print("Not acceptable")
-    break
+    if dollar.isalpha() or denom.isalpha() or nume.isalpha():
+        print("Invalid Price")
+        continue
+
+    else:
+        calc = int(dollar) * (int(nume) / int(denom)
+        print(shares,"shares with market price " + dollar + " " + nume + "/" + denom + " Have value", calc)
+
+        cont = input("Continue:")
+
+        if cont.upper() == "Y":
+            continue
+        else:
+            break
+    
