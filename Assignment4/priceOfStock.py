849bfaeb1f9654bea4563fd4c67a23fbd60b45a5
Styrkar Blaer <stykki@hotmail.com>
2018-09-25 13:13:40 +0000

    unknown mod

--- Assignment4/priceOfStock.py
+++ Assignment4/priceOfStock.py
@@ -1,25 +1,25 @@
 while True:
     while True:
         try:
-            shares = int(input("Number of shares: "))
+            shares = int(input("Enter number of shares: "))
 
         except ValueError:
-            print("Invalid Number!")
+            print("Invalid number!")
             continue
         else:
             break
     while True:
-        price = input("Enter a price (dollars, numerator, deniminator): ")
+        price = input("Enter price (dollars, numerator, denominator): ")
         dollar, nume, denom = price.split(" ")
 
         if dollar.isalpha() or denom.isalpha() or nume.isalpha():
-            print("Invalid Price!")
+            print("Invalid price!")
             continue
 
         else:
             calc = (float(dollar) + (float(nume) / float(denom))) * float(shares)
             calc = "%.2f" % calc
-            print(str(shares) + " shares with the market price " + str(dollar) + " " + str(nume) + "/" + str(denom) + " have value " + str(calc))
+            print(str(shares) + " shares with market price " + str(dollar) + " " + str(nume) + "/" + str(denom) + " have value $" + str(calc))
             break
 
     cont = input("Continue: ")
