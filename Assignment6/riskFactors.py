eb4951ef8a349677955ae5be6af93e816f373570
Styrkar Blaer <stykki@hotmail.com>
2018-10-09 08:06:33 +0000

    git commit -a 	modified:   ../Assignment6/riskFactors.py 	deleted:    ../test.txt 	../Assignment 1/Instructor Solution/ 	../Assignment2/Instructor Version/ 	../Assignment3/Instructor Version/ 	../Assignment4/Instructor Version/ 	../Assignment5/Instructor Version/ 	../Assignment6/CSV.txt 	./

--- Assignment6/riskFactors.py
+++ Assignment6/riskFactors.py
@@ -3,16 +3,23 @@ def get_csv(file):
     filecon = ""
     if os.path.isfile(file):
         with open(file,'r') as fin:
+            line = fin.readlines()
+            print(line[1])
             filecon = fin.read()
             return filecon
     else:
         print("Cannot find file: " + file)
 
 def seperation(thefile):
-    
+    for i in thefile.split(","):
+        i = i.strip("N/A")
+        i = i.strip("%")
+        print(i)
     return thefile
 
 files = input("Enter filename containing csv data: ")
 print("Indicator                        Min                              Max  ")
 print("---------------------------------------------------------------------------------------")
 seperation(get_csv(files))
+
+# I'm stumped, my man, i have no idea how to do this one.
